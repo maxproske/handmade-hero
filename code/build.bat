5e6ee1e99f800b7b6db2ba0e14bfbea8f8184168
Max Proske <mhcproske@gmail.com>
2018-06-16 22:07:11 -0700

    Add shell to build command

--- code/build.bat
+++ code/build.bat
@@ -1,5 +1,8 @@
 @echo off
 
+:: Run shell if cl is not a recognized command
+cl >NUL 2>&1 || ( call ../misc/shell.bat )
+
 :: `cl win32_handmade.cpp` builds an .obj (intermediate file) and .exe
 :: `-Zi` flag builds with .pdp debug files
 
